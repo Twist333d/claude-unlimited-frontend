7ceb22d48e83c098969911f56d9c2a6d99387176
Alexander Zuev <azuev@outlook.com>
2024-07-22 22:53:36 +0400

    Trying to fix the CORS

--- src/config.js
+++ src/config.js
@@ -2,4 +2,9 @@ const config = {
   apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:5001'
 };
 
+// Remove any trailing slash
+if (config.apiUrl.endsWith('/')) {
+  config.apiUrl = config.apiUrl.slice(0, -1);
+}
+
 export default config;
