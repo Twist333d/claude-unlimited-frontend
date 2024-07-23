c958e1c412842dd825a66de2f39b7bf81a7eb1d3
Alexander Zuev <azuev@outlook.com>
2024-07-23 09:43:12 +0400

    Installed Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'porcelain': '#F5F6F7',
+        'bali-hai': '#779EAD',
+        'fountain-blue': '#61AEC4',
+        'pale-sky': '#6F7681',
+        'oxford-blue': '#313947',
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+        'serif': ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
